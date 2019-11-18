24eb694696bf098d8a0d8e210bc14639463df2c8
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-11-18 14:45:20 -0800

    staging day 3-4

---
+++ blackjack/index.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+function cc(card) {
+ if (card = 2 || 3 || 4 || 5 || 6) {
+   console.log("ssss")
+ }
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
