221ae0d00f6c7855a51bad48cfa08ec38ef64677
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-11-18 15:02:11 -0800

    added addition and subtraction to count

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,15 +1,60 @@
+
 let count = 0;
 
 function cc(card) {
- if (card = 2 || 3 || 4 || 5 || 6) {
-   console.log("ssss")
- }
+  switch (card) {
+    case 2:
+      count += 1;
+      break;
+    case 3:
+      count += 1;
+      break;  
+    case 4:
+      count += 1;
+      break;
+    case 5:
+      count += 1;
+      break;
+    case 6:
+      count += 1;
+      break;
+    case 7:
+      count += 0;
+      break;
+    case 8:
+      count += 0;
+      break;
+    case 9:
+      count += 0;
+      break; 
+    case 10:
+        count -= 1;
+        break;
+    case 'J':
+      count -= 1;
+      break;
+    case 'Q':
+      count -= 1;
+      break;
+    case 'K':
+      count -= 1;
+      break;
+    case 'A':
+      count -= 1;
+      break;
+    default:
+        console.log("test");
+      break;
+  }
 
-
-  return "Change Me";
+  return count;
   // Only change code above this line
 }
 
 // Add/remove calls to test your function.
 // Note: Only the last will display
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
