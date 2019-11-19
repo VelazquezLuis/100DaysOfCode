0d6238f0b488350662480e49b30c007c6228ae1f
VelazquezLuis <javiervelazquez113@yahoo.com>
2019-11-19 09:32:34 -0800

    Finished day 5, with black jack game challenge.

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,6 +1,9 @@
 
 let count = 0;
-
+let play = "";
+function reset() {
+  count = 0 ;
+ }
 function cc(card) {
   switch (card) {
     case 2:
@@ -46,15 +49,23 @@ function cc(card) {
         console.log("test");
       break;
   }
-
-  return count;
+if (count <= 0) {
+  play = " Hold";
+} else if (count > 0 ) {
+  play = " Bet";
+}
+  return count + play;
   // Only change code above this line
 }
 
 // Add/remove calls to test your function.
 // Note: Only the last will display
-cc(2);
-cc(3);
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
 cc(7);
-cc("K");
-cc("A");
+cc(8);
+cc(9); 
+cc(2);
+cc(7);
